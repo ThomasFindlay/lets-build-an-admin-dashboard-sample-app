89f7c66bc52de1248cd3cb41a2bf203f78d76229
Thomas <thomasfindlay94@gmail.com>
2022-07-25 14:20:39 +0100

    Moved methods

--- src/stores/drawer.store.js
+++ src/stores/drawer.store.js
@@ -4,10 +4,10 @@ export const useDrawerStore = create((set, get) => {
   return {
     isDrawerOpen: true,
     isHoveringDrawer: false,
-    startHoveringDrawer: () => set({ isHoveringDrawer: true }),
-    stopHoveringDrawer: () => set({ isHoveringDrawer: false }),
     openDrawer: () => set({ isDrawerOpen: true }),
     closeDrawer: () => set({ isDrawerOpen: false }),
     toggleDrawer: () => set({ isDrawerOpen: !get().isDrawerOpen }),
+    startHoveringDrawer: () => set({ isHoveringDrawer: true }),
+    stopHoveringDrawer: () => set({ isHoveringDrawer: false }),
   };
 });
